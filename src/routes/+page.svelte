6c9597e3d0c98c457bae096f52acb30dea7b2195
Nicholas Stephan <nicholas.stephan@gmail.com>
2024-06-17 13:45:58 -0700

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,29 @@
 <script>
+	import { writable } from 'svelte/store';
 	import Article, { blocks } from '$lib/index.js';
 
-	// let value = [
-	// 	{
-	// 		id: 'paragraph',
-	// 		value:
-	// 			'0 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dapibus magna sed massa accumsan, sit amet dictum eros vestibulum. Donec ut tortor consectetur, fermentum odio vel, sagittis nisl. Maecenas eget ullamcorper libero. Curabitur turpis erat, consequat in aliquet eu, malesuada quis nibh. Nulla quis turpis leo. Curabitur nisi augue, porttitor a malesuada vitae, faucibus ut ipsum. Aliquam lobortis, eros sit amet dictum dapibus, massa quam feugiat neque, aliquam sollicitudin dolor felis ornare leo. Suspendisse potenti.'
-	// 	},
-	// 	{
-	// 		id: 'paragraph',
-	// 		value:
-	// 			'1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dapibus magna sed massa accumsan, sit amet dictum eros vestibulum. Donec ut tortor consectetur, fermentum odio vel, sagittis nisl. Maecenas eget ullamcorper libero. Curabitur turpis erat, consequat in aliquet eu, malesuada quis nibh. Nulla quis turpis leo. Curabitur nisi augue, porttitor a malesuada vitae, faucibus ut ipsum. Aliquam lobortis, eros sit amet dictum dapibus, massa quam feugiat neque, aliquam sollicitudin dolor felis ornare leo. Suspendisse potenti.'
-	// 	},
-	// 	{
-	// 		id: 'paragraph',
-	// 		value:
-	// 			'2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dapibus magna sed massa accumsan, sit amet dictum eros vestibulum. Donec ut tortor consectetur, fermentum odio vel, sagittis nisl. Maecenas eget ullamcorper libero. Curabitur turpis erat, consequat in aliquet eu, malesuada quis nibh. Nulla quis turpis leo. Curabitur nisi augue, porttitor a malesuada vitae, faucibus ut ipsum. Aliquam lobortis, eros sit amet dictum dapibus, massa quam feugiat neque, aliquam sollicitudin dolor felis ornare leo. Suspendisse potenti.'
-	// 	},
-	// 	{
-	// 		id: 'paragraph',
-	// 		value:
-	// 			'3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dapibus magna sed massa accumsan, sit amet dictum eros vestibulum. Donec ut tortor consectetur, fermentum odio vel, sagittis nisl. Maecenas eget ullamcorper libero. Curabitur turpis erat, consequat in aliquet eu, malesuada quis nibh. Nulla quis turpis leo. Curabitur nisi augue, porttitor a malesuada vitae, faucibus ut ipsum. Aliquam lobortis, eros sit amet dictum dapibus, massa quam feugiat neque, aliquam sollicitudin dolor felis ornare leo. Suspendisse potenti.'
-	// 	}
-	// ];
-	let value = [];
+	let value = [
+		{
+			id: 'paragraph',
+			value:
+				'0 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dapibus magna sed massa accumsan, sit amet dictum eros vestibulum. Donec ut tortor consectetur, fermentum odio vel, sagittis nisl. Maecenas eget ullamcorper libero. Curabitur turpis erat, consequat in aliquet eu, malesuada quis nibh. Nulla quis turpis leo. Curabitur nisi augue, porttitor a malesuada vitae, faucibus ut ipsum. Aliquam lobortis, eros sit amet dictum dapibus, massa quam feugiat neque, aliquam sollicitudin dolor felis ornare leo. Suspendisse potenti.'
+		},
+		{
+			id: 'paragraph',
+			value:
+				'1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dapibus magna sed massa accumsan, sit amet dictum eros vestibulum. Donec ut tortor consectetur, fermentum odio vel, sagittis nisl. Maecenas eget ullamcorper libero. Curabitur turpis erat, consequat in aliquet eu, malesuada quis nibh. Nulla quis turpis leo. Curabitur nisi augue, porttitor a malesuada vitae, faucibus ut ipsum. Aliquam lobortis, eros sit amet dictum dapibus, massa quam feugiat neque, aliquam sollicitudin dolor felis ornare leo. Suspendisse potenti.'
+		},
+		{
+			id: 'paragraph',
+			value:
+				'2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dapibus magna sed massa accumsan, sit amet dictum eros vestibulum. Donec ut tortor consectetur, fermentum odio vel, sagittis nisl. Maecenas eget ullamcorper libero. Curabitur turpis erat, consequat in aliquet eu, malesuada quis nibh. Nulla quis turpis leo. Curabitur nisi augue, porttitor a malesuada vitae, faucibus ut ipsum. Aliquam lobortis, eros sit amet dictum dapibus, massa quam feugiat neque, aliquam sollicitudin dolor felis ornare leo. Suspendisse potenti.'
+		},
+		{
+			id: 'paragraph',
+			value:
+				'3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dapibus magna sed massa accumsan, sit amet dictum eros vestibulum. Donec ut tortor consectetur, fermentum odio vel, sagittis nisl. Maecenas eget ullamcorper libero. Curabitur turpis erat, consequat in aliquet eu, malesuada quis nibh. Nulla quis turpis leo. Curabitur nisi augue, porttitor a malesuada vitae, faucibus ut ipsum. Aliquam lobortis, eros sit amet dictum dapibus, massa quam feugiat neque, aliquam sollicitudin dolor felis ornare leo. Suspendisse potenti.'
+		}
+	];
 </script>
 
 <Article {blocks} bind:value={value} editable={true} />
